cc602ecf6d2279d92ad108bd121045db4a0b3976
sauravjaiswa <sauravjaiswal520@gmail.com>
2021-05-07 01:25:30 +0530

    Database Connection initiated

---
+++ OnlineFoodDelivery.Data/OnlineFoodDeliveryDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineFoodDelivery.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineFoodDelivery.Data
+{
+    public class OnlineFoodDeliveryDbContext : DbContext
+    {
+        public OnlineFoodDeliveryDbContext(DbContextOptions<OnlineFoodDeliveryDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
